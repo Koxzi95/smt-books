ccde58ee3c0eeebcf8b35771290bd84bdb0d5321
Tom Cox <tom.jcox@icloud.com>
2014-10-19 13:28:11 +0100

    Validation change, needs looking at

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,5 @@ class Product < ActiveRecord::Base
   validates :title, :length => {:in => 2..35, :message => "must be between two and thirty five characters"}
   validates :description, :presence => { :message => "cannot be blank"}
   validates :description, :length => {:maximum => 2000, :message => "maximum length 2000 characters"}
-  validates :picture, :allow_blank => true, :format=> {:with => %r{\A(gif|jpg|png)\z}i,:multiline => true, :message => "Picture: must be a URL for GIF, JPG or PNG images."}
   validates :price, :numericality => {:greater_than_or_equal_to => 0, :allow_blank => false, :message => "must be a positive value"}
 end
